3742f3011c74191f303fa0d7bcfecfaaf3b7351b
AgnesYosefina <agnesyosefina941@gmail.com>
2023-12-02 18:10:24 +0700

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div className='text-center py-4 bg-yellow-800'>
+        <p className='font-bold'>copyright 2023 CodeSakti. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
